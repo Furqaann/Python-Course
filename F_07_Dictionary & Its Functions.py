25ab571d1f8816eccb7c9e7bfd03910d0cb50fe6
Furqan Fiaz <94382218+Furqaann@users.noreply.github.com>
2022-08-27 21:11:01 +0500

    Add files via upload

---
+++ F_07_Dictionary & Its Functions.py
@@ -0,0 +1,21 @@
+"""
+ Author: Furqan Fiaz
+ Date: 07-08-2022, 5.58 Pm
+ This code will contain the information about Dictionary & Its Functions etc
+"""
+# Dictionary is denoted by {} Brackets
+# We can call specific entity using key
+dy1={}
+dy2={1,2,3,4,5}
+dy3={"Ali":{"computer":25,"Maths":50,"English":30},"Ahmed":20,"Abdullah":22,"Furqan":50}
+print("What is the type of dy1:",type(dy1))
+print("The Result of Class is :",dy3)
+#We can print the result of specific entity, remember it is case sensitive
+print("The result of Furqan is:",dy3["Furqan"])
+
+#We can create a complete dictionary of an entity
+dy4={"Ali":{"computer":25,"Maths":50,"English":30}}
+print("the result of Ali is:",dy3["Ali"])
+
+
+
---
+++ F_08_Sets In Python.py
@@ -0,0 +1,19 @@
+"""
+ Author: Furqan Fiaz
+ Date: 06-08-2022, 6.06 Pm
+ This file will contain the information about Sets in Python
+"""
+# If we assign values to dictionary it also becomes set
+s1={1,2,3,4,5}
+print(type(s1))
+# We can define set by set()
+s2=set([1,2,3,4,5])
+print(type(s2))
+
+s3=set()
+s3.add(1)
+s3.add(2)
+s3.add(5)
+print(s3)
+
+# we can search functions of sets from Internet
