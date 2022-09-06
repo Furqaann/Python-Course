f097e626e7295cf99b522ab7080b81721bb553a5
Furqan Fiaz <94382218+Furqaann@users.noreply.github.com>
2022-09-06 13:51:46 +0500

    Add files via upload

---
+++ F_28_Abstraction&Encapsulation.py
@@ -0,0 +1,30 @@
+"""
+ Author: Furqan Fiaz
+ Date: 31-08-2022, 10.33 Pm
+ This file will contain the information about Abstraction and Encapsulation in Python
+"""
+
+# Abstraction
+"""
+Abstraction is each single entity performing specific task.
+Example: In a School, Teachers teach students, Students learn, Admin staff perform administrative duties
+Every one has a role and does not interfere in eachothers work.
+
+This complete School could be a layer of abstraction for another entity, 
+In a Complete Educational system, this school is a layer of abstraction 
+
+Dividing work in different things and developed by different persons and integrated in the end.
+"""
+# Encapsulation
+"""
+Implementation is hidden from the end user.
+If a user wants to run an addition function , he/she just needs to know that he/she has to enter 2 digits and enter 
+the addition button. Every thing else is npt important for user he/she doesn't need to know.
+
+"""
+if __name__ == '__main__':
+    Abstraction="Abstraction is each single entity performing specific task.\nExample: In a School, Teachers teach students, Students learn, Admin staff perform administrative duties\nEvery one has a role and does not interfere in eachothers work.\nThis complete School could be a layer of abstraction for another entity, \nIn a Complete Educational system, this school is a layer of abstraction \nDividing work in different things and developed by different persons and integrated in the end."
+    Encapsulation="Implementation is hidden from the end user.If a user wants to run an addition function \n he/she just needs to know that he/she has to enter 2 digits and enter the addition button.\n Every thing else is npt important for user he/she doesn't need to know."
+    print(Abstraction)
+    print(" ")
+    print(Encapsulation)
