4914af7d4fe83a839a0d974fb5e905ccfa08c937
Furqan Fiaz <94382218+Furqaann@users.noreply.github.com>
2022-09-15 16:20:59 +0500

    Addition File 36 (#27)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Addition File 30
    
    * Addition File 31
    
    * Addition File 32
    
    * Addition File 33
    
    * Addition File 34
    
    * Addition File 35
    
    * Addition File 36

---
+++ F_36_Object Instrospection.py
@@ -0,0 +1,15 @@
+""""
+ Author: Furqan Fiaz
+ Date:10-09-2022, 12.16 Am
+ This file will contain the information about Object Instrospection in Python
+"""
+# Everything in python is object based
+# Object instrospection is getting info about objects where it came from, which class does it belong to, etc
+a=34
+print(type(a))
+print(type("This is a string"))
+print(id("This is a string 1"))
+print(id("This is a string 2"))
+
+# We can do these things to our own defined objects of classes
+
